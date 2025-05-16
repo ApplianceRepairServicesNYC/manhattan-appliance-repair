0cc0724437bb765ce7f84d604b99aee63326ba0d
ApplianceRepairServicesNYC <fabianrivera2020@gmail.com>
2025-05-16 04:25:15 -0400

    Delete site.js

--- site.js
+++
@@ -1,3 +0,0 @@
-/* Placeholder for site.js */
-/* Replace this file with the actual JavaScript content from /js/site.js */
-/* Note: This file likely contains functions like ShowMenuDetails, HidePopup, Close_Form, etc. */
